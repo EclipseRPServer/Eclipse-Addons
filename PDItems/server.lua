d0f9ebe5ba92fb9b92d86850f19fbf5d52e3af91
Brandon <98152656+BrandonSnippets@users.noreply.github.com>
2022-12-30 10:17:45 -0600

    Removed esc menu

--- PDItems/server.lua
+++ PDItems/server.lua
@@ -5,8 +5,6 @@ RegisterNetEvent('pdcoffee:buy',function(data)
     local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.AddItem("pdcoffee", 1)
     Player.Functions.RemoveMoney('cash', 6, 'pdcoffee')
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["cash"], "remove", 6)
-    Wait(1000)
 	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["pdcoffee"], "add", 1)
 end)
 
@@ -15,9 +13,6 @@ RegisterNetEvent('pddonut:buy',function(data)
     local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.AddItem("rimjob", 1)
     Player.Functions.RemoveMoney('cash', 3, 'rimjob')
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["cash"], "remove", 3)
-
-    Wait(1000)
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["rimjob"], "add", 1)
 end)
 
