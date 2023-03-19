4b59c3a34d2b8992ebbc125f29702d2cf553ad5f
Bambi <96702617+Thewhiteguy8D@users.noreply.github.com>
2023-03-19 12:33:39 -0500

    updated spikes

--- nx-spikes/config.lua
+++ nx-spikes/config.lua
@@ -1,5 +1,5 @@
 Config = {
     RequireJobPlace = true, -- require job to place a spike strip?
-    JobRemove = 'police', -- job that can remove spike strip
+    JobRemove = 'police', 'bcso', 'sast' -- job that can remove spike strip
     Item = 'spikestrip' -- usable item 
 }
